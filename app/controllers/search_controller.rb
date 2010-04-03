1aaeea67eb2123f7064e7ec1704d675b89d769a6
Christian Ortner <chris.ortner@gmail.com>
2010-04-03 23:09:00 +0200

    added pagination for search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,17 @@
 class SearchController < ApplicationController
   
   def term_parser(term)
+    # dysfunctional!
+    user_id = /user:[a-zA-Z0-9]*/.match(term).to_s
+    if user_id != ''
+      user_id = user_id[5..user_id.length]
+      term += ' userId:' + Userentry.find_by_name(user_id).id.to_s
+    end
+    folder_id = /folder:[\/a-zA-Z0-9]*/.match(term).to_s
+    if folder_id != ''
+      folder_id = folder_id[7..folder_id.length]
+      term += ' folderId:' + Folderentry.find_by_name(folder_id).id.to_s
+    end    
     return term
   end
   
@@ -13,7 +24,8 @@ class SearchController < ApplicationController
 	    redirect_to :controller => 'search'
 	  else
 	    term = term_parser params[:query][:term]
-	    @file_results = Fileentry.find_with_ferret(params[:query][:term])
+      @prev_term = term
+	    @file_results = Fileentry.find_with_ferret(params[:query][:term], :page => params[:page], :per_page => 20)
 	    #@file_results = Fileentry.all(:conditions => {:name => params[:query][:term]})
 	    
 	    if @file_results == []
--- app/helpers/userentries_helper.rb
+++ app/helpers/userentries_helper.rb
@@ -1,2 +1,9 @@
 module UserentriesHelper
+  def parse_browse_url
+    
+  end
+  
+  def create_browse_url
+    
+  end
 end
