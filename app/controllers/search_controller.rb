6724159179a5d696c3702bd828e0c68649c8b926
Christian Ortner <chris.ortner@gmail.com>
2010-04-04 00:45:20 +0200

    working revision before attempting to group related files

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,27 @@
 class SearchController < ApplicationController
   
+  def search
+  end
+
+  def results
+    if not params[:query]
+	    flash[:error] = "No search terms specified!"
+	    redirect_to :controller => 'search'
+	  else
+	    term = term_parser params[:query][:term]
+      @prev_term = term
+	    @file_results = Fileentry.find_with_ferret params[:query][:term], :page => params[:page], :per_page => 20
+	    #@file_results = group_identical_files @file_results
+	    
+	    if @file_results == []
+	      flash[:error] = "No results for this search term!"
+	      redirect_to :action => 'search'
+	    end
+	  end
+  end
+
+
+  private
   def term_parser(term)
     # dysfunctional!
     user_id = /user:[a-zA-Z0-9]*/.match(term).to_s
@@ -15,24 +37,18 @@ class SearchController < ApplicationController
     return term
   end
   
-  def search
+  def group_identical_files(fileentry_collection)
+    # Takes elements of the collection and groups them into arrays in a hash based on their hashCode.
+    hash = {}
+    for file in fileentry_collection do
+      if hash[file].class == Array
+        # may need improvement later
+        hash[file] += file
+      else
+        hash[file] = []
+      end
+    end
+    
+    return hash
   end
-
-  def results
-    if not params[:query]
-	    flash[:error] = "No search terms specified!"
-	    redirect_to :controller => 'search'
-	  else
-	    term = term_parser params[:query][:term]
-      @prev_term = term
-	    @file_results = Fileentry.find_with_ferret(params[:query][:term], :page => params[:page], :per_page => 20)
-	    #@file_results = Fileentry.all(:conditions => {:name => params[:query][:term]})
-	    
-	    if @file_results == []
-	      flash[:error] = "No results for this search term!"
-	      redirect_to :action => 'search'
-	    end
-	  end
-  end
-
 end
--- app/helpers/userentries_helper.rb
+++ app/helpers/userentries_helper.rb
@@ -1,9 +1,2 @@
 module UserentriesHelper
-  def parse_browse_url
-    
-  end
-  
-  def create_browse_url
-    
-  end
 end
